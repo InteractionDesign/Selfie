9ad361d976ac6f0140dae66f5a886075b48251d7
Felix <felixhedlund@me.com>
2014-03-28 15:33:00 +0100

    Added ShakeDetector

--- src/com/example/selfi/WelcomeActivity.java
+++ src/com/example/selfi/WelcomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.selfi;
 
+import com.example.selfi.ShakeDetector.OnShakeListener;
+
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,6 +19,9 @@ import android.widget.Button;
 public class WelcomeActivity extends Activity {
 	
 	public Button button;
+	private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
 
     @Override
@@ -27,16 +35,47 @@ public class WelcomeActivity extends Activity {
             public void onClick(View v) {
             	Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
             	 startActivityForResult(intent, 0); 
-                
-
-          
             }
     });
+        
+     // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new OnShakeListener() {
+ 
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+                handleShakeEvent(count);
+            }
+
+			private void handleShakeEvent(int count) {
+				Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+				startActivityForResult(intent, 0);
+			}
+        });
     }
     
-    
-
-
+   
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,    SensorManager.SENSOR_DELAY_UI);
+    }
+ 
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
