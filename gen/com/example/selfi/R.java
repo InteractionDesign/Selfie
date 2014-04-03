6607091ec634b6a37f5b3f808976a1102debe6bf
Felix <felixhedlund@me.com>
2014-04-03 14:29:35 +0200

    Display pictures

--- gen/com/example/selfi/R.java
+++ gen/com/example/selfi/R.java
@@ -25,21 +25,28 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button=0x7f080001;
-        public static final int camera_preview=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int button=0x7f080002;
+        public static final int button1=0x7f080003;
+        public static final int camera_preview=0x7f080001;
+        public static final int imageviewid=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_welcome=0x7f030001;
+        public static final int activity_display_picture=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_welcome=0x7f030002;
     }
     public static final class menu {
-        public static final int welcome=0x7f070000;
+        public static final int display_picture=0x7f070000;
+        public static final int welcome=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int choose_photo=0x7f050004;
+        public static final int hello_world=0x7f050006;
         public static final int take_photo=0x7f050003;
+        public static final int title_activity_display_picture=0x7f050005;
         public static final int welcome=0x7f050002;
     }
     public static final class style {
--- src/com/example/selfi/WelcomeActivity.java
+++ src/com/example/selfi/WelcomeActivity.java
@@ -2,10 +2,13 @@ package com.example.selfi;
 
 import com.example.selfi.ShakeDetector.OnShakeListener;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.view.Menu;
@@ -19,6 +22,9 @@ public class WelcomeActivity extends Activity {
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
     private ShakeDetector mShakeDetector;
+    private String selectedImagePath;
+    private static final int SELECT_PICTURE = 1;
+    public static final String EXTRA_MESSAGE = "com.example.selfi";
 
 
     @Override
@@ -59,6 +65,25 @@ public class WelcomeActivity extends Activity {
                 startActivityForResult(intent, 0);
             }
         });
+        
+        ((Button) findViewById(R.id.button1))
+        .setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
+        
+        
+        
+        
     }
     
    
@@ -81,6 +106,43 @@ public class WelcomeActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.welcome, menu);
         return true;
+    }
+   
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+                Intent i = new Intent(WelcomeActivity.this, DisplayPictureActivity.class);
+                i.putExtra(EXTRA_MESSAGE, selectedImagePath);
+				startActivity(i);
+				
+                
+            }
+        }
+    }
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+            // just some safety built in 
+            if( uri == null ) {
+                // TODO perform some logging or show user feedback
+                return null;
+            }
+            // try to retrieve the image from the media store first
+            // this will only work for images selected from gallery
+            String[] projection = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+            if( cursor != null ){
+                int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                return cursor.getString(column_index);
+            }
+            // this is our fallback here
+            return uri.getPath();
     }
     
 }
