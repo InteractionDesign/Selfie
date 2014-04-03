5a39495265e39242f5bd74203d8d1d9ab21d0f75
CatarinaSorensen <catarinasorensen1991@gmail.com>
2014-04-03 23:13:52 +0200

    Simple tilt functionality

--- src/com/example/selfi/WelcomeActivity.java
+++ src/com/example/selfi/WelcomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.selfi;
 
+
 import com.example.selfi.ShakeDetector.OnShakeListener;
 
 import android.net.Uri;
@@ -10,139 +11,203 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class WelcomeActivity extends Activity {
-	
-    public Button button;
-    private SensorManager mSensorManager;
-    private Sensor mAccelerometer;
-    private ShakeDetector mShakeDetector;
-    private String selectedImagePath;
-    private static final int SELECT_PICTURE = 1;
-    public static final String EXTRA_MESSAGE = "com.example.selfi";
+public class WelcomeActivity extends Activity implements SensorEventListener {
+	private SensorManager sensorManager;
+
+	public Button button;
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
+	private String selectedImagePath;
+	private static final int SELECT_PICTURE = 1;
+	public static final String EXTRA_MESSAGE = "com.example.selfi";
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_welcome);
-        button = (Button)findViewById(R.id.button);
-        button.setOnClickListener(new OnClickListener() {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-            public void onClick(View v) {
-                //Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-            	//startActivityForResult(intent, 0); 
-            	Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
+		setContentView(R.layout.activity_welcome);
+		button = (Button)findViewById(R.id.button);
+		button.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				//Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+				//startActivityForResult(intent, 0); 
+				Intent i = new Intent(WelcomeActivity.this, MainActivity.class);
 				startActivity(i);		
-            }
-    });
-        
-        // ShakeDetector initialization
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAccelerometer = mSensorManager
-                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mShakeDetector = new ShakeDetector();
-        mShakeDetector.setOnShakeListener(new OnShakeListener() {
- 
+			}
+		});
+
+		// ShakeDetector initialization
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+		mAccelerometer = mSensorManager
+				.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mShakeDetector = new ShakeDetector();
+
+		//Removed this functionality from WelcomeActivity
+		/* mShakeDetector.setOnShakeListener(new OnShakeListener() {
+
             @Override
-            public void onShake(int count) {
-                /*
-                 * The following method, "handleShakeEvent(count):" is a stub //
-                 * method you would use to setup whatever you want done once the
-                 * device has been shook.
-                 */
-                handleShakeEvent(count);
+            public void onShake(int count) {*/
+		/*
+		 * The following method, "handleShakeEvent(count):" is a stub //
+		 * method you would use to setup whatever you want done once the
+		 * device has been shook.
+		 */
+		/*         handleShakeEvent(count);
             }
 
             private void handleShakeEvent(int count) {
                 Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
                 startActivityForResult(intent, 0);
             }
-        });
-        
-        ((Button) findViewById(R.id.button1))
-        .setOnClickListener(new OnClickListener() {
+        });*/
 
-            public void onClick(View arg0) {
 
-                // in onCreate or any event where your want the user to
-                // select a file
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent,
-                        "Select Picture"), SELECT_PICTURE);
-            }
-        });
-        
-        
-        
-        
-    }
-    
-   
-    @Override
-    public void onResume() {
-        super.onResume();
-        // Add the following line to register the Session Manager Listener onResume
-        mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
-    }
- 
-    @Override
-    public void onPause() {
-        // Add the following line to unregister the Sensor Manager onPause
-        mSensorManager.unregisterListener(mShakeDetector);
-        super.onPause();
-    }
+		((Button) findViewById(R.id.button1))
+		.setOnClickListener(new OnClickListener() {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.welcome, menu);
-        return true;
-    }
-   
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                Uri selectedImageUri = data.getData();
-                selectedImagePath = getPath(selectedImageUri);
-                Intent i = new Intent(WelcomeActivity.this, DisplayPictureActivity.class);
-                i.putExtra(EXTRA_MESSAGE, selectedImagePath);
+			public void onClick(View arg0) {
+
+				// in onCreate or any event where your want the user to
+				// select a file
+				Intent intent = new Intent();
+				intent.setType("image/*");
+				intent.setAction(Intent.ACTION_GET_CONTENT);
+				startActivityForResult(Intent.createChooser(intent,
+						"Select Picture"), SELECT_PICTURE);
+			}
+		});
+
+
+
+		sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+		// add listener. The listener will be HelloAndroid (this) class
+		sensorManager.registerListener(this, 
+				sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+				SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+
+	}
+
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		// Add the following line to register the Session Manager Listener onResume
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+	}
+
+	@Override
+	public void onPause() {
+		// Add the following line to unregister the Sensor Manager onPause
+		mSensorManager.unregisterListener(mShakeDetector);
+		super.onPause();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.welcome, menu);
+		return true;
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == SELECT_PICTURE) {
+				Uri selectedImageUri = data.getData();
+				selectedImagePath = getPath(selectedImageUri);
+				Intent i = new Intent(WelcomeActivity.this, DisplayPictureActivity.class);
+				i.putExtra(EXTRA_MESSAGE, selectedImagePath);
 				startActivity(i);
-				
-                
-            }
-        }
-    }
 
-    /**
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-            // just some safety built in 
-            if( uri == null ) {
-                // TODO perform some logging or show user feedback
-                return null;
-            }
-            // try to retrieve the image from the media store first
-            // this will only work for images selected from gallery
-            String[] projection = { MediaStore.Images.Media.DATA };
-            Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-            if( cursor != null ){
-                int column_index = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                return cursor.getString(column_index);
-            }
-            // this is our fallback here
-            return uri.getPath();
-    }
-    
+
+			}
+		}
+	}
+
+	/**
+	 * helper to retrieve the path of an image URI
+	 */
+	public String getPath(Uri uri) {
+		// just some safety built in 
+		if( uri == null ) {
+			// TODO perform some logging or show user feedback
+			return null;
+		}
+		// try to retrieve the image from the media store first
+		// this will only work for images selected from gallery
+		String[] projection = { MediaStore.Images.Media.DATA };
+		Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+		if( cursor != null ){
+			int column_index = cursor
+					.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+			cursor.moveToFirst();
+			return cursor.getString(column_index);
+		}
+		// this is our fallback here
+		return uri.getPath();
+	}
+
+	public void onAccuracyChanged(Sensor sensor,int accuracy){
+
+	}
+
+
+
+	public void onSensorChanged(SensorEvent event){
+
+
+		// check sensor type
+		if(event.sensor.getType()==Sensor.TYPE_ACCELEROMETER){
+
+			// assign directions
+			float x=event.values[0];
+			float y=event.values[1];
+			float z=event.values[2];
+
+
+
+
+			if(x<-5){
+				Context context = WelcomeActivity.this;
+				String text ="right";
+
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast.makeText(context, text, duration).show();
+			}
+			if(x>5){
+				Context context = WelcomeActivity.this;
+				String text ="left";
+
+				int duration = Toast.LENGTH_SHORT;
+
+				Toast.makeText(context, text, duration).show();
+			}
+			if(z<0){
+				Context context = WelcomeActivity.this;
+				String text ="forward";
+
+				int duration = Toast.LENGTH_SHORT;
+
+
+				Toast.makeText(context, text, duration).show();
+			}
+		}
+
+	}
 }
