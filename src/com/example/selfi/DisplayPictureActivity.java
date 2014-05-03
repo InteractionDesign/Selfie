2a3967ce47572b1090050fd72b42923fdf67067a
Felix <felixhedlund@me.com>
2014-05-03 10:09:56 +0200

    Added shake on DisplayPictureActivity

--- src/com/example/selfi/DisplayPictureActivity.java
+++ src/com/example/selfi/DisplayPictureActivity.java
@@ -2,6 +2,9 @@ package com.example.selfi;
 
 import java.io.File;
 
+import com.example.selfi.ShakeDetector.OnShakeListener;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 
@@ -9,12 +12,22 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Build;
 
 public class DisplayPictureActivity extends Activity {
+
+	protected static final int SPLASH_SCREEN = 3;
+	private static final int CAMERA_PICTURE = 2;
+	private static final String EXTRA_MESSAGE = "com.example.selfi";
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private ShakeDetector mShakeDetector;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +35,37 @@ public class DisplayPictureActivity extends Activity {
 		setContentView(R.layout.activity_display_picture);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
+
 		Intent intent = getIntent();
 		String imagePath = intent.getStringExtra(WelcomeActivity.EXTRA_MESSAGE);
-		
-		File imgFile = new  File(imagePath);
-		if(imgFile.exists()){
 
-		    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+		File imgFile = new File(imagePath);
+		if (imgFile.exists()) {
 
-		    ImageView myImage = (ImageView) findViewById(R.id.imageviewid);
-		    myImage.setImageBitmap(myBitmap);
+			Bitmap myBitmap = BitmapFactory.decodeFile(imgFile
+					.getAbsolutePath());
+
+			ImageView myImage = (ImageView) findViewById(R.id.imageviewid);
+			myImage.setImageBitmap(myBitmap);
 
 		}
-	    
+		
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mShakeDetector = new ShakeDetector();
+
+		mShakeDetector.setOnShakeListener(new OnShakeListener() {
+			@Override
+			public void onShake(int count) {
+
+				Intent i = new Intent(DisplayPictureActivity.this,
+						SplashScreen.class);
+				startActivityForResult(i, SPLASH_SCREEN);
+				
+
+			}
+		});
+
 	}
 
 	/**
@@ -65,4 +95,33 @@ public class DisplayPictureActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == CAMERA_PICTURE) {
+				String photoPath = data.getStringExtra("photoPath");
+				Intent i = new Intent(DisplayPictureActivity.this,
+						DisplayPictureActivity.class);
+				i.putExtra(EXTRA_MESSAGE, photoPath);
+				startActivity(i);
+
+			} else if (requestCode == SPLASH_SCREEN) {
+				Intent i = new Intent(DisplayPictureActivity.this,
+						MainActivity.class);
+				startActivityForResult(i, CAMERA_PICTURE);
+
+			}
+		}
+	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(mShakeDetector);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		mSensorManager.registerListener(mShakeDetector, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+
+	}
 }
