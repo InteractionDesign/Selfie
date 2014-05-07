40419ce6b4debe6af9e61b8160fc307fea397934
Felix <felixhedlund@me.com>
2014-05-07 14:19:56 +0200

    Filter + shake camera logo

--- src/com/example/selfi/DrawStuff.java
+++ src/com/example/selfi/DrawStuff.java
@@ -4,20 +4,40 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 public class DrawStuff extends View {
-	private Bitmap shakecamera;
+  private Bitmap shakecamera;
+  private double width;
+  private double height;
+  private boolean action;
 
-	public DrawStuff(Context context) {
-		super(context);
-		shakecamera = BitmapFactory.decodeResource(getResources(),R.drawable.shakecamera); 
-	}
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);	
-
-		canvas.drawBitmap(shakecamera, 180, 900, null);
-	}	
+  public DrawStuff(Context context) {
+    super(context);
+    shakecamera = BitmapFactory.decodeResource(getResources(),R.drawable.shakecamera); 
+    WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+    Display display = wm.getDefaultDisplay();
+    Point size = new Point();
+    display.getSize(size);
+    width = size.x;
+    height = size.y;
+    action = false;
+  }
+  public void actionStarted(boolean bol){
+    if(bol){
+      action = true;
+    }
+  }
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas); 
+    double new_height = height - 0.2*height;
+    if (!action){
+    canvas.drawBitmap(shakecamera, (float) width/4, (float) new_height, null);
+    }
+  } 
 
 }
