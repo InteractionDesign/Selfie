505a58a420efefaf8931a3d71bbbe1fff73b295a
Felix <felixhedlund@me.com>
2014-05-08 11:52:27 +0200

    fix tilt picture

--- src/com/example/selfi/DrawStuff.java
+++ src/com/example/selfi/DrawStuff.java
@@ -62,7 +62,7 @@ public DrawStuff(Context context, int i) {
         if(activity == 1){
         	if(!action){
         		canvas.drawBitmap(tilt_left, (float) ((float) width-width+0.05*width), (float) height/2, null);
-        		canvas.drawBitmap(tilt_right, (float) ((float) width-0.35*width), (float) height/2, null);
+        		canvas.drawBitmap(tilt_right, (float) ((float) width-0.3*width), (float) height/2, null);
         	}
         }
     }
